ecc11fd1225a0b6f78f4835e6f7a9adf41595daf
Paul Browne <Paulmatthewbrowne@gmail.com>
2019-07-11 16:36:58 +0300

    Update runWhenUntil.js

--- runWhenUntil.js
+++ runWhenUntil.js
@@ -1,20 +1,19 @@
-/*! runWhenUntil v1.2.0 | MIT License | github.com/paul-browne/runWhenUntil */ 
+/*! runWhenUntil v1.2.1 | MIT License | github.com/paul-browne/runWhenUntil */ 
 function runWhenUntil(args){
     var id = setInterval(function(){
-        var _when = args.when && args.when();
         var _until = args.until && args.until();
-        if(!args.when){
+        if(args.when){
+            if( args.when() ){
+                args.run();
+            }
+            if( args.when() || _until ){
+                clearInterval(id);
+            }
+        }else{
             if( _until ){
                 clearInterval(id);
             }else{
                 args.run();
-            }
-        }else{
-            if( _when ){
-                args.run();
-            }
-            if( _when || _until ){
-                clearInterval(id);
             }
         }
     }, (1000 / args.perSecond) || 50);
